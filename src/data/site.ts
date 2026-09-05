d6284c056007a47e1b780a09be797ed5379d0455
matthew-foote <ut.foote@gmail.com>
2026-09-05 17:13:32 -0600

    Add site data

---
+++ src/data/site.ts
@@ -0,0 +1,37 @@
+// Edit this file to update links and video IDs. Everything else picks it up.
+export const site = {
+  name: 'Matthew Foote',
+  tagline: 'I build hardware for flight.',
+  subline:
+    'I design hardware, prototype it, and iterate toward flight. First-year mechanical engineering student at BYU, building a liquid ethanol rocket motor in Onshape. Previously took a full-scale XC cockpit prototype from CAD to print.',
+  emails: ['ut.foote@gmail.com', 'footmat@byu.edu'],
+  github: 'https://github.com/matthew-foote',
+  linkedin: 'https://www.linkedin.com/in/matthew-foote/',
+  instagram: '', // optional
+  resumePath: '/resume/Matthew_Foote_Resume.pdf',
+  // Hero spec strip
+  specs: [
+    { label: 'Focus', value: 'Propulsion, structures, DFM' },
+    { label: 'CAD', value: 'Onshape, Fusion 360' },
+    { label: 'Shop', value: 'FDM, CNC, assembly' },
+  ],
+  // YouTube video IDs (the part after v= in the URL). Leave '' to show a placeholder.
+  youtube: {
+    nica: 'zYyErTfU5E8',
+    waterRocket: 'cTAZamTHgY8', // 9th-grade 3D-printed water rocket launch
+    sony: '2rqw14xSyM0',
+    blender: 'vs8xvEHFLoY',
+    kizik: '', // upload the Kizik film, paste its ID here
+    arcboat: '', // upload the Arc boat film as Unlisted, paste its ID here
+  },
+  links: {
+    jobPosting: 'https://job-boards.greenhouse.io/spacex/jobs/8553649002',
+    kizikPost: 'https://www.linkedin.com/feed/update/urn:li:activity:7218630434534871041/',
+    kizikDeck: 'https://docs.google.com/presentation/d/1sR8OQjptbetWOoMpv0GVXQuo2RdQQjuhxRccP398TWQ/',
+    enginePost: 'https://www.linkedin.com/feed/update/urn:li:activity:7493900446063996928/',
+    onshape:
+      'https://cad.onshape.com/documents/2e7de27d412da306461ff66c/w/b4ad1ae2201e4b2162e6cd96/e/b9ea5142a431c49cc3eecec8',
+    thingiverse: 'https://www.thingiverse.com/thing:3648749',
+    enlisted: '', // Enlisted's website, if you want to link it
+  },
+};
